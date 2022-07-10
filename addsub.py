06933ee10eb37a28542e7afc129b8616959bf457
HiteshMahajan7797 <hiteshmahajan7797@gmail.com>
2022-07-10 15:19:05 +0530

    changes done

---
+++ addsub.py
@@ -0,0 +1,19 @@
+# Store input numbers
+num1 = 10
+num2 = 4
+
+# Add two numbers
+sum = int(num1) + int(num2)
+
+# Display the sum
+print('The Sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# Store input numbers
+num1 = 6
+num2 = 2
+
+# Sub two numbers
+sub = int(num1) - int(num2)
+
+# Display the sum
+print('The Subtract of {0} and {1} is {2}'.format(num1, num2, sub))
